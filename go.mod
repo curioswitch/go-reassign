curioswitch/go-noreassign

First commit of go linter that checks for sentinel error reassignment

go.mod
@@ -1,0 +1,10 @@
+module github.com/anuraaga/reassign
+
+go 1.17
+
+require golang.org/x/tools v0.1.11
+
+require (
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+)

main.go
@@ -1,0 +1,11 @@
+package main
+
+import (
+	"golang.org/x/tools/go/analysis/singlechecker"
+
+	"github.com/anuraaga/reassign/internal/analyzer"
+)
+
+func main() {
+	singlechecker.Main(analyzer.New())
+}

curioswitch/go-reassign

go.mod
@@ -1,0 +1,10 @@
+module github.com/curioswitch/reassign
+
+go 1.17
+
+require golang.org/x/tools v0.1.11
+
+require (
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+)

main.go
@@ -1,0 +1,11 @@
+package main
+
+import (
+	"golang.org/x/tools/go/analysis/singlechecker"
+
+	"github.com/curioswitch/reassign/internal/analyzer"
+)
+
+func main() {
+	singlechecker.Main(analyzer.New())
+}